d701b70a6573dd7eee09b2d8e70d496415efa255
Wojto <wlacheta@interia.pl>
2018-08-08 20:30:38 +0200

    JSON refactor, esp reset after wrong states

--- ESP32Regulator/ESP32Regulator.ino
+++ ESP32Regulator/ESP32Regulator.ino
@@ -10,46 +10,47 @@
 #include <PubSubClient.h>
 
 const char* CONFIG_FILE = "/config.json";
-bool checkbut_status = false; //stan checkboxa
-bool if_write = false;
-const char* mqtt_server = NULL;
-const char* mqtt_port = NULL; //docelowo musi byc uint16 (moze byc int)
-const char* mqtt_user = NULL;
-const char* mqtt_pass = NULL;
+bool checkbut_status = false; 
+const char* mqtt_server;
+const char* mqtt_port; //uint16 but it could be 'int'
+const char* mqtt_user;
+const char* mqtt_pass;
 
 //JSON handling functions
 bool ReadFile();
 void WriteFile();
 
-//obsluga clienta MQTT
+//MQTT subscriber
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
    Serial.begin(115200);
 
-   //obsluga pliku systemowego
+   //file system handling
     bool result = SPIFFS.begin();
     Serial.println("SPIFFS is opened:" + result);
-
-   if(!ReadFile()) {
+     
+    if(!ReadFile()) {
      Serial.println("Failed to read config file, using default values");
    }
+  
 }
 
 void loop() {
-   //tworzenie checkboxa w html
+   //html checkbox
    char customhtml[24] = "type=\"checkbox\"";
    
    if(checkbut_status) {
-    strcat(customhtml, " checked"); //jezeli checkbox true to dopisujemy stan "checked"
+     strcat(customhtml, " checked"); //only way to handle checkbox state is to add "checked" string
    }
+
    
-   // po wcisnieciu button przepisuje wartosc do pola tekstowego
+   //JavaScript functions
    WiFiManagerParameter skrypt_java("<script language=\"JavaScript\"> function funkcja1(){var asd=document.getElementById('select1').options[document.getElementById('select1').selectedIndex].value; var macOutput=document.getElementById(\"macOutput\"); macOutput.value=asd;}</script>");
    WiFiManagerParameter mac_searcher("<select id=\"select1\"> <option value=\"0\">0</option> <option value=\"1\">1</option> <option value=\"2\">2</option></select><input type=\"text\" id=\"macOutput\"/><input type=\"button\" value=\"Pobierz\" onclick=\"funkcja1()\">");
      
-   //Tworzenie parametrow na stronie konfiguracyjnej
+   //Creating custom HTML
    WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
    WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
    WiFiManagerParameter custom_mqtt_user("user", "mqtt user", mqtt_user, 20);
@@ -66,7 +67,7 @@ void loop() {
     
     wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
-    //dodanie pol 
+    //adding custom html to config page
     wifiManager.addParameter(&custom_mqtt_server);
     wifiManager.addParameter(&custom_mqtt_port);
     wifiManager.addParameter(&custom_mqtt_user);
@@ -81,25 +82,28 @@ void loop() {
     wifiManager.addParameter(&line);
     wifiManager.addParameter(&skrypt_java);
     
-    //laczenie do AP 
+    //Connecting to AP
     if(wifiManager.autoConnect("ESP 32 - test","wojtek2468")) {
+
+      checkbut_status = (strncmp(save_checkbox.getValue(),"T",1) == 0); //0==0, checkbut_status = true -> odczytana wczensiej wartosc (z JSONA) jest taka sama, checkbox zaznaczony;
+
+      if(checkbut_status) {
        mqtt_server = custom_mqtt_server.getValue();
        mqtt_port = custom_mqtt_port.getValue();
        mqtt_user = custom_mqtt_user.getValue();
        mqtt_pass = custom_mqtt_pass.getValue();
+      }
+      else {
+       mqtt_server = "";
+       mqtt_port = "";
+       mqtt_user = "";
+       mqtt_pass = "";
+      }
 
-    Serial.println("connected...yeey :)"); //po tej komendzie jestesmy polaczeni z wifi
-
-    if(checkbut_status = (strncmp(save_checkbox.getValue(),"T",1) == 0)) { //0==0, checkbut_status = true -> odczytana wczensiej wartosc (z JSONA) jest taka sama, checkbox zaznaczony;
-      if_write = true;
-      WriteFile();    
-    }
-    else {
-      if_write = false;
-      WriteFile();
-    }
+    WriteFile();   
+    Serial.println("connected...yeey :)"); //After this we are connected to wifi network 
     
-    //ustawienie serwera cloudmqtt
+    //Set CloudMQTT access
     client.setServer(custom_mqtt_server.getValue(), atoi(custom_mqtt_port.getValue()));
    
     //serwer mqqt
@@ -113,15 +117,19 @@ void loop() {
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
- 
+      ESP.restart();
+      delay(5000);
     }
   }
 
   client.publish(custom_mqtt_topic.getValue(), "wersja 2");
   
-  } //zamkniecie if(wifiManager.autoConnect("ESP 32 - test","wojtek2468"))
+  } else {  //zamkniecie if(wifiManager.autoConnect("ESP 32 - test","wojtek2468"))
+    ESP.restart();
+    delay(5000);
+  }
   
-//podtrzymanie polaczenia z mqtt  
+//Holding connection with MQTT 
 client.loop();
 }
 
@@ -136,8 +144,7 @@ void WriteFile() {
   json["mqtt_port"] = mqtt_port;
   json["mqtt_user"] = mqtt_user;
   json["mqtt_pass"] = mqtt_pass;
-  json["if_write"] = if_write;
-
+ 
   //zapis parametrow do pliku
   File f = SPIFFS.open(CONFIG_FILE, "w");
   if(!f) {
@@ -177,7 +184,6 @@ bool ReadFile() {
 
     //zapisywanie z jsona do zmiennych
     if(json.containsKey("checkbut_status")) checkbut_status = json["checkbut_status"]; 
-    if(json.containsKey("if_write")) if_write = json["if_write"];
     if(json.containsKey("mqtt_server")) mqtt_server = json["mqtt_server"];
     if(json.containsKey("mqtt_port")) mqtt_port = json["mqtt_port"];
     if(json.containsKey("mqtt_user")) mqtt_user = json["mqtt_user"];
